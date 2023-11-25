9f034fc87f5d00eccd916a9739edc6e1d6aead85
Andrea Di Paola <andr3a.dipaola@gmail.com>
2023-11-25 21:35:14 +0100

    added tailwind ad conditional classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import "./Navbar.css";
+import react from "../assets/react.svg";
 
 const Navbar = () => {
-  const x = 999;
+  const x = 9;
+  const y = 11;
   const img = "vite";
-  const img2 = "react";
   return (
     <>
       <div>{x > 1000 ? "sopra 1000" : "sotto 1000"}</div>
       <div>{x.toFixed(3)}</div>
       <img src={`/${img}.svg`} alt="" />
-      <img src={`../assets/${img2}.svg`} alt="" />
-      <img src="../assets/react.svg" alt="" />
+      <img style={{ height: "80px" }} src={react} alt="" />
       <div className={`box rounded ${x < 10 ? "rotated" : ""}`}>x é {x}</div>
+      <div className="bg-red-500 rounded-lg w-25 h-25 rotate-45">Pima</div>
+      <div className={`rounded-lg w-25 h-25 ${y < 10 ? "bg-red-500 rotate-45" : "bg-sky-500"}`}>Dopo</div>
       <ul>
         <li>Hello World</li>
         <li>Hello World</li>
